9156fd00e23bd82297dd32245d4ac0afda2f3d23
Joris <joris.44@live.fr>
2016-01-21 17:21:22 +0100

    Exo 8 : Manipulation d'une liste d'éléments au sein d'un layout

--- app/src/main/java/fr/android/androidexercises/BookAdapter.java
+++ app/src/main/java/fr/android/androidexercises/BookAdapter.java
@@ -1,39 +1,56 @@
 package fr.android.androidexercises;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 
 import java.util.List;
 
 public class BookAdapter extends BaseAdapter {
 
     private final List<Book> books;
+    private final LayoutInflater inflater;
 
     public BookAdapter(Context context, List<Book> books) {
         this.books = books;
         // TODO LayoutInflater.from()
+        inflater = LayoutInflater.from(context);
     }
 
     @Override
     public int getCount() {
         // TODO
+        return books.size();
     }
 
     @Override
     public Book getItem(int position) {
         // TODO
+        return books.get(position);
     }
 
     @Override
     public long getItemId(int position) {
         // TODO
+        return books.get(position).hashCode();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // TODO
+        BookItemView bookItemView;
+        if (convertView == null) {
+            bookItemView = (BookItemView) inflater.inflate(R.layout.custom_view_item_book, parent,
+                    false);
+        } else {
+            bookItemView = (BookItemView) convertView;
+        }
+        Book book = books.get(position);
+        bookItemView.bindView(book);
+        return bookItemView;
     }
 
 }
